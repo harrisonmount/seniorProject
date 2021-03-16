9aaff0cf1de84dbf8014e61f87719859ef26728b
harrisonmount <51917836+harrisonmount@users.noreply.github.com>
2021-03-16 16:12:00 -0400

    Update file reader.py

--- senior/NewsEmbedding/file reader.py
+++ senior/NewsEmbedding/file reader.py
@@ -3,47 +3,45 @@ import pandas as pd
 import json
 import os
 import re
-
+import numpy as np
 
 def readDB(database):
 
-    origpath = "C:\\Users\\minim\\OneDrive\\Desktop\\pheme rumors\\pheme-rumour-scheme-dataset\\threads\\en\\"
+    origpath = "/Users/harrisonmount/Desktop/School/Senior Project/Repo/SeniorPrj/pheme-rumour-scheme-dataset/threads/en/"
     path = "{opath}{db}".format(opath=origpath, db=database)
-    cols = ['contributors', 'truncated', 'text', 'in_reply_to_status_id', 'id', 'favorite_count',
-                               'source', 'retweeted', 'coordinates', 'entities', 'in_reply_to_screen_name', 'id_str',
-                               'retweet_count', 'in_reply_to_user_id', 'favorited', 'user', 'geo',
-                               'in_reply_to_user_id_str', 'possibly_sensitive', 'lang', 'created_at', 'filter_level',
-                               'in_reply_to_status_id_str', 'place', 'metadata', 'extended_entities']
-    df = pd.DataFrame(columns=cols)
+    newpath = os.path.join(path, "user_matrix.csv")
+    dict = {} #dictionary of users : [list of who they follow]
 
-    with open("source_tweets.csv", 'w', encoding="utf-8") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=cols)
-        writer.writeheader()
-        rows = []
-        for subdir, dirs, files in os.walk(path):
-            for filename in files:
-                if filename.endswith(".json"):
-                    if "source-tweets" in subdir or "reactions" in subdir:
-                        fpath = os.path.join(subdir, filename)
-                        print(os.path.join(subdir, filename))
-                        f = open(fpath, "r")
-                        dict = json.load(f)
-                        dict['text'] = re.sub(r"[\n\t]*", "", dict['text'])
-                        rows.append(dict)
+    #MAKE A NON REPEATING LIST OF ALL USERS
 
-                        #print(dict)
-                        #temp = pd.DataFrame.from_dict(dict)
-                        #print(temp['id'])
-                        #df = df.append(temp, ignore_index=True)
-                        #print(dict.keys())
-                        f.close()
-        writer.writerows(rows)
-    csvfile.close()
+    for subdir, dirs, files in os.walk(path):
+        for filename in files:#loop down each tweet directory
+            if filename.endswith("who-follows-whom.txt"):
+                fpath = os.path.join(subdir, "who-follows-whom.dat")
+                npath = os.path.join(subdir, "who-follows-whom.txt")
+                #os.rename(fpath, npath) #USE IF who-follows-whom are still .dat files, change above to endswith("who-follows-whom.dat")
+                f = open(npath, "r")
+                for line in f:
+                    x = line.split()#separate the line into two integers
+                    if x[0] in dict:#if key already exists, add user theyre following to [], if not make new key with that value
+                        dict[x[0]].append(x[1])
+                    else:
+                        dict[x[0]] = [x[1]]
 
-    #f = open(path, "r")
+                f.close()
 
-    #f.close()
-
+    individualusers = dict.keys()
+    print("individual user list created...\nmaking csv file with size diffusers/diffusers")
+    df = pd.DataFrame(columns=individualusers)
+    df.insert(0,0,individualusers)
+    print('adding values to matrix')
+    for itemno in individualusers:
+        list = dict[itemno]
+        for following in list:
+            df.at[itemno, following] = 1
+            print("adding 1 to: [" + itemno + ", "+ following +"]")
+    print("done")
+    #print(df)
     return 0
 
 
